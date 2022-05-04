fd428c6f14762d339f99a34d4d6493d96278113b
Michal <sikora1.michal1@gmail.com>
2022-05-04 08:52:56 +0200

    i

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//PROGRAM 1) 4 LEKCE  ---  OK  ---
+
+        Scanner sc = new Scanner(System.in);
+      //  System.out.println("Zadej jmeno: ");
+      //  String jmeno = (sc.nextLine());
+      //  System.out.println("Zadej prijmeni: ");
+      //  String prijmeni = (sc.nextLine());
+      //  System.out.println("Zadej vek: ");
+      //  int vek = (sc.nextInt());
+      //  System.out.println("tve jmeno " + jmeno.toUpperCase()+ " " + prijmeni.toUpperCase());
+      //  System.out.println("Za rok ti bude " + vek+1);
+
+//PROGRAM 2) 4 LEKCE  ---  DODELAT NEVIM PREVOD STRINGU NA INT  ---
+
+      //  System.out.println("Zadej dve slova první delsi a druhy kratsi:");
+      //  String a = (sc.nextLine());
+      //  System.out.println("Delsi slovo:");
+      //  String b = (sc.nextLine());
+      //  String aa = a.trim();
+      //  int aaa = Integer.parseInt(a.trim());
+      //  String bb = b.trim();
+      //  int bbb = Integer.parseInt(b.trim());
+      //  System.out.println("Slova se lisi o " + (aaa - bbb)  + "znaku");
+
+//PROGRAM 3) 4 LEKCE  ---  OK  ---
+
+      //  System.out.println("Zadej text kde se objevi retezec itnetwork");
+      //  String a = (sc.nextLine());
+      //  System.out.println("Zadal jsi: " + a);
+      //  System.out.println(a.contains("itnetwork"));
+
+
+    }
+}
